1e286d06d3c221bf2cdf780622eaa61c07b9814c
whynot-byoul <whynot.byoul@gmail.com>
2024-05-25 13:59:26 +0900

    주소 변경

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 // 배포 시 baseURL proxy 로 바꿔주기
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
-    // baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
+    // baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
+    baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
     headers: {
-        'Content-Type': 'application/json',
-        authorization: 'Bearer ' + sessionStorage.getItem('token'),
+        "Content-Type": "application/json",
+        authorization: "Bearer " + sessionStorage.getItem("token"),
     },
 });
 /**
@@ -13,22 +13,22 @@ const api = axios.create({
  */
 api.interceptors.request.use(
     (request) => {
-        console.log('Starting Request', request);
+        console.log("Starting Request", request);
         return request;
     },
     function (error) {
-        console.log('REQUEST ERROR', error);
+        console.log("REQUEST ERROR", error);
     }
 );
 
 api.interceptors.response.use(
     (response) => {
-        console.log('Response:', response);
+        console.log("Response:", response);
         return response;
     },
     function (error) {
         error = error.response.data;
-        console.log('RESPONSE ERROR', error);
+        console.log("RESPONSE ERROR", error);
         return Promise.reject(error);
     }
 );
